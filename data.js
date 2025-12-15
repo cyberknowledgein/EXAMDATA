2507917b3a132581d5492be277346e12b176a4ce
Cyber Knowledge <cyberknowledgeteam@gmail.com>
2025-12-15 21:17:14 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const studentData = [
+  {
+    name: "DINESH MIRDHA",
+    mobile: "9348006277",
+    class: "X",
+    percentage: "56",
+    status: "PASS",
+    remarks: "Excellent awareness and participation"
+  },
+  {
+    name: "Rohit Das",
+    mobile: "9123456789",
+    class: "X",
+    grade: "C",
+    status: "FAIL",
+    remarks: "Needs improvement"
+  }
+];
